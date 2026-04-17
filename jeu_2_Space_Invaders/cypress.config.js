d0e6e28ef0259ef149a69e38e10623ea3f4b6525
Aurelien Deutchoua <aurelien.deutchoua@epitech.eu>
2026-04-17 16:48:39 +0200

    modif du fichier cypress.config

--- jeu_2_Space_Invaders/cypress.config.js
+++ jeu_2_Space_Invaders/cypress.config.js
@@ -1,7 +1,10 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:1234',
+    supportFile: false, 
+    setupNodeEvents(on, config) {
+      
+    },
   },
 });
